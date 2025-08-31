e1b3e102018f50f0ffa893f062df90f8d30344de
asyasocute <asya_bububu@proton.me>
2025-09-01 00:47:48 +0500

    works

--- main.go
+++ main.go
@@ -1,44 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"image"
+	"io"
+	"os"
+	"path"
+	"sync"
 
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
-	"io"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 
 	_ "golang.org/x/image/bmp"
+	// _ "golang.org/x/image/ico"
+	// _ "golang.org/x/image/svg"
 	_ "golang.org/x/image/webp"
 
 	"github.com/gocolly/colly"
 )
 
-func saveFile(img *http.Response, path string) {
-	file, err := os.Create(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-	_, err = io.Copy(file, img.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("saved to", path)
-}
+// var mu sync.Mutex
+var wg sync.WaitGroup
 
 func main() {
 	c := colly.NewCollector(
-		colly.MaxDepth(1),
-		colly.Async(true),
+		colly.MaxDepth(2),
+		colly.Async(false),
 	)
-	i := 0
-	c.Limit(&colly.LimitRule{DomainGlob: "*.neocities.org", Parallelism: 4})
+
+	// i := 0
+	c.Limit(&colly.LimitRule{DomainGlob: "*.neocities.org", Parallelism: 8})
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
@@ -50,27 +45,54 @@ func main() {
 
 	c.OnHTML("img[src]", func(e *colly.HTMLElement) {
 		link := e.Attr("src")
+		url := e.Request.AbsoluteURL(link)
 		absoluteURL := e.Request.AbsoluteURL(link)
-		response, err := http.Get(absoluteURL)
+		wg.Add(1)
+
+		// go func(url string) {
+		defer wg.Done()
+		response, err := http.Get(url)
 		if err != nil {
 			fmt.Println("error??", err)
 			return
 		}
+		fileName := path.Base(url)
 		defer response.Body.Close()
-		m, _, err := image.Decode(response.Body)
+		data, err := io.ReadAll(response.Body)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to get image %s: %v\n", absoluteURL, err)
+			fmt.Fprintf(os.Stderr, "failed to read body %s: %v\n", url, err)
 			return
 		}
+
+		m, _, err := image.Decode(bytes.NewReader(data))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to get image %s: %v\n", url, err)
+			return
+		}
+
 		bounds := m.Bounds()
 		if bounds.Dx() == 88 && bounds.Dy() == 31 {
-			fmt.Println("found badge", "./tmp/"+strconv.Itoa(i))
-			saveFile(response, "./tmp/"+strconv.Itoa(i))
-			i += 1
+			fmt.Println("found badge", fileName)
+			// mu.Lock()
+			// i += 1
+			// mu.Unlock()
+			c.Visit(absoluteURL)
+			file, err := os.Create("./tmp/" + fileName)
+			if err != nil {
+				panic("failed to create file " + fileName)
+			}
+			defer file.Close()
+
+			_, err = file.Write(data)
+			if err != nil {
+
+			}
+
 		}
+
 		// fmt.Printf("%s %d %d\n", link, m.Width, m.Height)
-		fmt.Println("img ", absoluteURL)
 		// e.Request.Visit(link)
+		// }(absoluteURL)
 	})
 	c.OnError(func(r *colly.Response, e error) {
 		log.Println("error:", e, r.Request.URL, string(r.Body))
@@ -78,8 +100,9 @@ func main() {
 
 	// Start scraping on https://en.wikipedia.org
 	// c.Visit("https://ranfren.neocities.org/")
-	// c.Visit("https://asyasocute.online/")
-	c.Visit("http://localhost:4321/")
+	c.Visit("https://asyasocute.online/")
+	// c.Visit("http://localhost:4321/")
 	// Wait until threads are finished
 	c.Wait()
+	wg.Wait()
 }
